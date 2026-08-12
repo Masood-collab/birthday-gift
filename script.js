8a4359415028d4453087316902052adebfbd805c
Masood-collab <hassanmasood44600@gmail.com>
2026-08-12 21:01:49 +0500

    Add openGift function to display birthday message

---
+++ script.js
@@ -0,0 +1,5 @@
+function openGift() {
+    document.getElementById("message").innerHTML =
+        "🎉 Happy Birthday! 🎂✨<br><br>" +
+        "May your day be filled with happiness, smiles and beautiful memories! 💖";
+}
